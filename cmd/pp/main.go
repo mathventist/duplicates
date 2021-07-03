ffefbc03270a98dc3254ed638fc7389ef97a6e55
mathventist <mathventist@gmail.com>
2021-07-02 18:39:42 -0600

    Added usage, flags for input file to the pp util

--- cmd/pp/main.go
+++ cmd/pp/main.go
@@ -9,6 +9,22 @@ import (
 	"github.com/mathventist/duplicates"
 )
 
+var removeStops bool
+var fileName string
+
+func init() {
+	const (
+		defaultRemoveStops = false
+		defaultFileName    = ""
+	)
+
+	flag.BoolVar(&removeStops, "r", defaultRemoveStops, "remove stop words from text")
+	flag.BoolVar(&removeStops, "removeStops", defaultRemoveStops, "remove stop words from text")
+
+	flag.StringVar(&fileName, "f", defaultFileName, "input filename")
+	flag.StringVar(&fileName, "file", defaultFileName, "input filename")
+}
+
 func main() {
 	flag.Usage = func() {
 		usageText := `pp - a utility for normalizing text for further comparison.
@@ -18,26 +34,42 @@ It removes titles, numerics, hyphens, and internal sentence punctuation, expands
 Optionally, this utility will also strip out English stop words (see https://gist.github.com/sebleier/554280).
 
 USAGE
-  $ pp
+  $ pp [ -h | --help ] [ -f <filename> | --file <filename> ] [ -r | --removeStops ]
 
 OPTIONS
-  -r remove stop words from the text
+  -f, --file         input filename. Standard input when omitted
+  -r, --removeStops  remove stop words from the text
+  -h, --help         print the help message
 
 EXAMPLES
   $ echo "testing 1 i 2 me 3 normalizing" | pp -r
   testing normalizing
+
+  $ pp -f myfile
   `
 
 		fmt.Println(usageText)
 	}
 
-	removeStops := flag.Bool("r", false, "remove stop words from text")
 	flag.Parse()
 
+	var inputErr error
 	file := os.Stdin
+
+	if len(fileName) > 0 {
+		file, inputErr = os.Open(fileName)
+
+		if inputErr != nil {
+			fmt.Fprintln(os.Stderr, "error opening input file: ", inputErr)
+			flag.Usage()
+
+			os.Exit(1)
+		}
+	}
+
 	fi, err := file.Stat()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "stdin error", err)
+		fmt.Fprintln(os.Stderr, "stdin error: ", err)
 		flag.Usage()
 
 		os.Exit(1)
@@ -46,7 +78,7 @@ EXAMPLES
 	// Exit if stdin is empty.
 	size := fi.Size()
 	if size == 0 {
-		fmt.Fprintln(os.Stderr, "error: standard input is empty.")
+		fmt.Fprintln(os.Stderr, "standard input is empty")
 		flag.Usage()
 
 		os.Exit(1)
@@ -54,7 +86,7 @@ EXAMPLES
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Fprintln(os.Stdout, duplicates.Preprocess(scanner.Text(), *removeStops))
+		fmt.Fprintln(os.Stdout, duplicates.Preprocess(scanner.Text(), removeStops))
 	}
 
 	os.Exit(0)

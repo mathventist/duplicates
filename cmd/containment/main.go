dd43335cd53c5787f719e65213effd7f61cac1e0
mathventist <mathventist@gmail.com>
2021-07-04 16:53:51 -0600

    Added usage and exit codes to the containment cmd

--- cmd/containment/main.go
+++ cmd/containment/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -9,28 +10,56 @@ import (
 )
 
 func main() {
-	args := os.Args
+	flag.Usage = func() {
+		usageText := `containment - a utility for computing the containment of one document within another.
 
-	if len(args) < 3 {
-		fmt.Println("missing operand")
-		return
+Given two documents A and B and their respective sets of ngrams, S(A), S(B), the measure to which document B is contained in document A, C(A,B), is defined as:
+
+  C(A,B) = |S(A) ∩ S(B)| / |S(B)|
+
+Input files must each contain a single ngram per line, and the ngrams must all be the same size for an accurate calculation.
+
+The output is a floating point value, greater or equal to 0.0 and less than or equal to 1.0. A value of 1.0 indicates complete containment.
+
+USAGE
+  $ containment [ -h | --help ] <file A> <file B>
+
+OPTIONS
+  -h, --help  print the help message
+
+EXAMPLES
+  $ containment fileA fileB
+  0.3
+  `
+
+		fmt.Println(usageText)
 	}
 
-	if len(args) > 3 {
-		fmt.Println("too many operands")
-		return
+	flag.Parse()
+
+	args := flag.Args()
+
+	if flag.NArg() != 2 {
+		fmt.Fprintln(os.Stderr, "invalid number of arguments; please provide two file names.")
+		flag.Usage()
+
+		os.Exit(1)
 	}
 
-	a, err := duplicates.FileToArray(args[1])
+	a, err := duplicates.FileToArray(args[0])
 	if err != nil {
-		fmt.Printf("error processing %s: %v\n", args[1], err)
-		return
+		fmt.Fprintln(os.Stderr, "error processing file: ", err)
+		flag.Usage()
+
+		os.Exit(1)
 	}
 
-	b, err := duplicates.FileToArray(args[2])
+	b, err := duplicates.FileToArray(args[1])
 	if err != nil {
-		fmt.Printf("error processing %s: %v\n", args[2], err)
-		return
+		fmt.Fprintln(os.Stderr, "error processing file: ", err)
+		flag.Usage()
+
+		os.Exit(1)
 	}
 
 	sa := set.New()
@@ -43,6 +72,6 @@ func main() {
 	}
 
 	intersection := sa.Intersection(sb)
-
-	fmt.Printf("Containment is %v\n", float64(intersection.Len())/float64(sb.Len()))
+	fmt.Fprintf(os.Stdout, "%v\n", float64(intersection.Len())/float64(sb.Len()))
+	os.Exit(0)
 }

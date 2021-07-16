fb8ad9408882c974d30a49ea2593aef2b20a8704
mathventist <mathventist@gmail.com>
2021-07-15 20:03:32 -0600

    Keep ref to original sentence when comparing them in compareEquals

--- cmd/compareEqual/main.go
+++ cmd/compareEqual/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"text/tabwriter"
@@ -9,56 +11,85 @@ import (
 )
 
 func main() {
-	args := os.Args
-
-	if len(args) < 3 {
-		fmt.Println("missing operand")
-		return
+	flag.Usage = func() {
 	}
 
-	if len(args) > 3 {
-		fmt.Println("too many operands")
-		return
+	flag.Parse()
+
+	args := flag.Args()
+
+	if flag.NArg() != 2 {
+		fmt.Fprintln(os.Stderr, "invalid number of arguments; please provide two file names.")
+		flag.Usage()
+
+		os.Exit(1)
 	}
 
-	a, err := duplicates.FileToArray(args[1])
-	if err != nil {
-		fmt.Printf("error processing %s: %v\n", args[1], err)
-		return
-	}
+	c := populateSliceFromFile(args[0])
+	d := populateSliceFromFile(args[1])
 
-	b, err := duplicates.FileToArray(args[2])
-	if err != nil {
-		fmt.Printf("error processing %s: %v\n", args[2], err)
-		return
-	}
-
-	results := compare(a, b)
+	a, b := <-c, <-d
+	results := compare(a, b, true)
 
 	// Display results
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-	fmt.Fprintln(w, "File\tNumber of sentences\t% of duplicate sentences")
-	fmt.Fprintf(w, "%v\t%v\t%v\n", args[1], len(a), 100*len(results)/len(a))
-	fmt.Fprintf(w, "%v\t%v\t%v\n", args[2], len(b), 100*len(results)/len(b))
+	fmt.Fprintln(w, "File\tNumber of sentences\tPercentage of matched sentences")
+	fmt.Fprintf(w, "%v\t%v\t%v\n", args[0], len(a), 100*len(results)/len(a))
+	fmt.Fprintf(w, "%v\t%v\t%v\n", args[1], len(b), 100*len(results)/len(b))
 	w.Flush()
 
-	fmt.Printf("\n\n%v duplicate sentences in total:\n\n", len(results))
-	for i, d := range results {
-		fmt.Printf("%v: %v\n\n", i+1, d)
+	fmt.Printf("\n\n%v sentences matches in total:\n\n", len(results))
+	for _, r := range results {
+		fmt.Fprintf(os.Stdout, "%v\n", r)
 	}
-
 }
 
-func compare(a []string, b []string) []string {
-	var results []string
+type indexedString struct {
+	Index  int
+	String string
+}
 
-	for _, aa := range a {
-		for _, bb := range b {
-			if duplicates.Preprocess(aa, false) == duplicates.Preprocess(bb, false) {
-				results = append(results, aa)
+func compare(a []string, b []string, removeStops bool) [][2]indexedString {
+	var results [][2]indexedString
+
+	for i, aa := range a {
+		for j, bb := range b {
+			if duplicates.Preprocess(aa, removeStops) == duplicates.Preprocess(bb, removeStops) {
+				var match [2]indexedString
+				match[0] = indexedString{i, aa}
+				match[1] = indexedString{j, bb}
+
+				results = append(results, match)
 			}
 		}
 	}
 
 	return results
+}
+
+func populateSliceFromFile(fileName string) <-chan []string {
+	c := make(chan []string)
+
+	go func() {
+		f, err := os.Open(fileName)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error opening file: ", err)
+			flag.Usage()
+
+			os.Exit(1)
+		}
+		defer f.Close()
+
+		fileScanner := bufio.NewScanner(f)
+		fileScanner.Split(duplicates.ScanSentences)
+
+		var lines []string
+		for fileScanner.Scan() {
+			lines = append(lines, fileScanner.Text())
+		}
+
+		c <- lines
+	}()
+
+	return c
 }
